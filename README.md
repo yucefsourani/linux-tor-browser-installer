a4952a44dcc300c44b254fbc2518e0a04bff0f30
yucefsourani <youssef.m.sourani@gmail.com>
2017-08-11 20:33:15 +0300

    v0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# linux-tor-browser-installer
+# linux-tor-browser-installer 0.2
 Python Script To Install Tor Browser
 
 www.arfedora.blogspot.com
@@ -8,12 +8,12 @@ www.arfedora.blogspot.com
  *  ```python3 ```
  * ``` python3-gnupg ```
  * ``` python3-beautifulsoup4 ```
-
-
+ * ``` python3-requests ```
+ * ``` tor ```
 
 
 # Install Requires (Fedora)
- * ``` sudo dnf install git python3-beautifulsoup4 python3-gnupg ```
+ * ``` sudo dnf install git tor python3-requests python3-beautifulsoup4 python3-gnupg ```
 
 
 
@@ -23,3 +23,9 @@ www.arfedora.blogspot.com
  * ``` chmod 755 ~/linux-tor-browser-installer/linux-tor-browser-installer.py ```
 
  * ``` ~/linux-tor-browser-installer/linux-tor-browser-installer.py```
+ 
+ 
+# Options
+ * ``` --without-check-sig || -s       ==> Install without Check File Signature  ```
+ * ``` --without-tor       || -t       ==> Download Tor Browser without Tor Network ```
+
--- linux-tor-browser-installer.py
+++ linux-tor-browser-installer.py
@@ -22,17 +22,22 @@
 #  
 # python3-gnupg
 # python3-beautifulsoup4
+# python3-requests
+# tor
 
 
 from bs4 import BeautifulSoup
-import urllib
 from os.path import join,basename,isdir,isfile
 from os import uname,system,geteuid,chdir,getcwd,remove
 from pwd import getpwuid
 from shutil import rmtree
 import tarfile
 import gnupg
-
+import requests
+import sys
+import dbus
+import subprocess
+import json
 
 def import_key():
 	gpg=gnupg.GPG(gnupghome=join(home,".gnupg"))
@@ -53,32 +58,101 @@ def extract_tar_xz(location):
 	with tarfile.open(location) as f:
 		f.extractall(home)
 		
-def dm(url,name,size,location,sym="#",symb="-",symx="[",symy="]"):
+
+def dm(opurl,name,size,location,sym="#",symb="-",symx="[",symy="]",chunk_size = 600):
 	old_dir = getcwd()
 	chdir(location)
-	url        = urllib.request.Request(url,headers={"User-Agent":"Mozilla/5.0"})
-	opurl      = urllib.request.urlopen(url)
 	openfile   = open(name,"wb")
-	block_size = 600
 	psize = 0
 	print (symx+symb*100+symy+" "+str(size)+"b"+" "+"0%",end="\r",flush=True)
-	while True:
-		count = int((psize*100)//size)
-		n = sym * count
-		bfr = opurl.read(block_size)
-		openfile.write(bfr)
-		psize += block_size
-		print (symx+n+symb*(100-count)+symy+" "+str(size)+"b"+" "+str(round((psize*100)/size,2))+"%",end="\r",flush=True)
-		if psize >= size:
-			break
+	with open(name, 'wb') as op:
+		for chunk in opurl.iter_content(chunk_size=chunk_size):
+			count = int((psize*100)//size)
+			n = sym * count
+			op.write(chunk)
+			psize += chunk_size
+			print (symx+n+symb*(100-count)+symy+" "+str(size)+"b"+" "+str(round((psize*100)/size,2))+"%",end="\r",flush=True)
 			
-	openfile.close()
 	print (" "*200,end="\r",flush=True)
 	print (symx+sym*100+symy+" "+str(size)+"b"+" "+"100%")
 	chdir(old_dir)
 	
 	
+def check_tor_service():
+	result = ""
+	bus = dbus.SystemBus()
+	proxy = bus.get_object("org.freedesktop.systemd1","/org/freedesktop/systemd1")
+	interface = dbus.Interface(proxy,"org.freedesktop.systemd1.Manager")
+	m = interface.get_dbus_method("ListUnitFiles")
+	all_unit_files = m()
+	for s in all_unit_files:
+		if s[0].endswith("tor.service"):
+			result = "ff"
+			
+	if result=="ff":
+		if not service_is_started():
+			check = subprocess.call("pkexec systemctl start tor.service",shell=True)
+			if check !=0:
+				return "f"
+
+	else:
+		return "n"
+	
+	return "s"
+	
+
+def service_is_started():
+	result = ""
+	bus = dbus.SystemBus()
+	proxy = bus.get_object("org.freedesktop.systemd1","/org/freedesktop/systemd1")
+	interface = dbus.Interface(proxy,"org.freedesktop.systemd1.Manager")
+	m = interface.get_dbus_method("GetUnit")
+	try:
+		m("tor.service")
+	except:
+		return False
+	return True
+	
+def get_real_ip(status=False):
+	url = "http://httpbin.org/ip"
+	if status:
+		opurl = session.get(url).json()
+	else:
+		opurl = requests.get(url).json()
+	return opurl["origin"]
+	
 if __name__ == "__main__":
+	help_ = """Version : 0.2
+
+linux-tor-browser-installer --without-check-sig || -s       ==> Install without Check File Signature
+
+linux-tor-browser-installer --without-tor  || -t            ==> Download Tor Browser without Tor Network
+"""
+	if len(sys.argv)>1:
+		if "--without-check-sig" in sys.argv or "-s" in sys.argv:
+			check_sig = False
+		else:
+			check_sig = True
+		if "--without-tor" in  sys.argv or "-t" in sys.argv:
+			with_tor = False
+		else:
+			with_tor = True
+		if "--help" in sys.argv or "-h" in sys.argv:
+			exit(help_)
+	else:
+		check_sig = True
+		with_tor  = True
+	
+	
+	if with_tor:
+		session = requests.session()
+		session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                   'https': 'socks5://127.0.0.1:9050'}
+	else:
+		session = requests
+	
+	
+	agent = {"User-Agent":"Mozilla/5.0"}
 	system('setterm -cursor off')
 
 	arch              = uname().machine
@@ -114,7 +188,7 @@ StartupWMClass=Tor Browser
 		while True:
 			system("clear")
 			print ("\nNothing To Do.")
-			print ("Q To Quit || F To Force Install Tor Browser || R To Remove Tor Browser.")
+			print ("Q To Quit || F To Force Reinstall Tor Browser || R To Remove Tor Browser.")
 			answer = input("- ").strip()
 			if answer == "q" or answer == "Q":
 				exit("\nBye...")
@@ -131,26 +205,41 @@ StartupWMClass=Tor Browser
 			elif answer == "f" or answer == "F":
 				print ("\n")
 				break
-
-	url = urllib.request.Request("https://www.torproject.org/download/download-easy.html.en",headers={"User-Agent":"Mozilla/5.0"})
-	opurl = urllib.request.urlopen(url)
-	soup = BeautifulSoup(opurl,"html.parser")
+	
+	
+	if  with_tor:
+		print ("Old Real IP ====> {}".format(get_real_ip()))
+		print("[#]Check/Start Tor Service.")
+		check = check_tor_service()
+		if  check=="f":
+			exit ("-Start Tor Service Fail.")
+		elif check == "n":
+			exit ("-Error Please Install Tor.")
+		else:
+			print ("New Real IP ====> {}".format(get_real_ip(True)))
+			print ("-Sucess.\n")
+			
+	url = "https://www.torproject.org/download/download-easy.html.en"
+	opurl = session.get(url,headers=agent)
+	soup = BeautifulSoup(opurl.text,"html.parser")
 
 
 	for a in soup.findAll("a",{"class":button_class}):
-		url_tor = urllib.request.Request(join("https://www.torproject.org",a.get("href")[3:]),headers={"User-Agent":"Mozilla/5.0"})
-		opurl_tor = urllib.request.urlopen(url_tor)
-		tor = basename(url_tor.full_url)
-		size  = opurl_tor.headers["Content-Length"]
 		tor_download = join("https://www.torproject.org",a.get("href")[3:])
+		opurl_tor = session.get(tor_download,stream=True,headers=agent)
+		tor = basename(opurl_tor.url)
+		size  = opurl_tor.headers["Content-Length"]
+
+
+
 
 		
 	for a in soup.findAll("a",{"class":sig}):
-		url_tor_sig = urllib.request.Request(join("https://www.torproject.org",a.get("href")[3:]),headers={"User-Agent":"Mozilla/5.0"})
-		opurl_tor_sig = urllib.request.urlopen(url_tor_sig)
-		tor_sig = basename(url_tor_sig.full_url)
-		size_sig = opurl_tor_sig.headers["Content-Length"]
 		tor_download_sig = join("https://www.torproject.org",a.get("href")[3:])
+		opurl_tor_sig = session.get(tor_download_sig,stream=True,headers=agent)
+		tor_sig = basename(opurl_tor_sig.url)
+		size_sig = opurl_tor_sig.headers["Content-Length"]
+
 
 				
 		
@@ -158,7 +247,7 @@ StartupWMClass=Tor Browser
 	if not isfile(join(home,"Downloads",tor)):
 		print("[#]get {} Size {}".format(tor_download,size))
 		try:
-			dm(tor_download,tor,int(size),location=join(home,"Downloads"))
+			dm(opurl_tor,tor,int(size),location=join(home,"Downloads"))
 			print("\n")
 		except:
 			exit("\nDownloading {} To {} Fail.".format(tor_download,join(home,"Downloads")))
@@ -169,27 +258,24 @@ StartupWMClass=Tor Browser
 	if not isfile(join(home,"Downloads",tor_sig)):
 		print("[#]get {} Size {}".format(tor_download_sig,size_sig))
 		try:
-			dm(tor_download_sig,tor_sig,int(size_sig),location=join(home,"Downloads"))
+			dm(opurl_tor_sig,tor_sig,int(size_sig),location=join(home,"Downloads"))
 			print("\n")
 		except:
 			exit("\nDownloading {} To {} Fail.".format(tor_download_sig,join(home,"Downloads")))
 	
 	
 	
+	if check_sig:
+		print ("[#]Start Import GPG Key.")
+		if not import_key():
+			exit("Import GPG Key Fail.")
+		else:
+			print ("-Import GPG Key Done.\n")
 	
-	print ("[#]Start Import GPG Key.")
-	if not import_key():
-		exit("Import GPG Key Fail.")
-	else:
-		print ("-Import GPG Key Done.\n")
-	
-	
-	
-	
-	print ("[#]Start Check Valid signatures.")
-	if not check_valid_sg(join(home,"Downloads",tor_sig),join(home,"Downloads",tor)):
-		exit("WARNING! Check Valid signatures Fail.")
-	print ("-Check Valid signatures Done.\n")
+		print ("[#]Start Check Valid signatures.")
+		if not check_valid_sg(join(home,"Downloads",tor_sig),join(home,"Downloads",tor)):
+			exit("WARNING! Check Valid signatures Fail.")
+		print ("-Check Valid signatures Done.\n")
 	
 	
 	

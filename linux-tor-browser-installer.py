cb01855ec66d54559d603a12544605ffd020d7f3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 00:53:04 +0000

    Adding tarfile member sanitization to extractall()

--- linux-tor-browser-installer.py
+++ linux-tor-browser-installer.py
@@ -58,7 +58,26 @@ def check_valid_sg(stream,data_file):
 	
 def extract_tar_xz(location):
 	with tarfile.open(location) as f:
-		f.extractall(home)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(f, home)
 		
 
 def dm(opurl,name,size,location,sym="#",symb="-",symx="[",symy="]",chunk_size = 600):
